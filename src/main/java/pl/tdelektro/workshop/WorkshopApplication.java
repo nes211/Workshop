32246d88d7fcbfb8a291c5142536986a4356c9ac
TomTD <nes211nes211nes@gmail.com>
2024-02-15 07:56:17 +0100

    Workshop helper project init

---
+++ src/main/java/pl/tdelektro/workshop/WorkshopApplication.java
@@ -0,0 +1,18 @@
+package pl.tdelektro.workshop;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
