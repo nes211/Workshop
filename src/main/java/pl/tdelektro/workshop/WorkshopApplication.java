6de19796ea459786b17f45bbd18a61ec13242cc7
TomTD <nes211nes211nes@gmail.com>
2024-03-11 09:54:10 +0100

    xml parsing from file to Task class

--- src/main/java/pl/tdelektro/workshop/WorkshopApplication.java
+++ src/main/java/pl/tdelektro/workshop/WorkshopApplication.java
@@ -1,16 +1,15 @@
 package pl.tdelektro.workshop;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.opencsv.CSVReader;
-import com.opencsv.ICSVParser;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import pl.tdelektro.workshop.pojo.Car;
 import pl.tdelektro.workshop.pojo.Task;
 import pl.tdelektro.workshop.pojo.User;
@@ -20,9 +19,6 @@ import pl.tdelektro.workshop.repository.UserRepository;
 import pl.tdelektro.workshop.security.Admin;
 
 import java.io.FileReader;
-import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @SpringBootApplication
@@ -46,49 +42,39 @@ public class WorkshopApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-
-
-
-
-        List<User> userList = Arrays.asList(
-                new User("workshop@tdelektro.pl", adminPassword.getPassword(), "ADMIN"),
-                new User("tom@test.com", passwordEncoder.encode("password1")),
-                new User("dawid@test.com", passwordEncoder.encode("password2")),
-                new User("adalbert@test.com", passwordEncoder.encode("password3"))
-        );
-
-        for (User user : userList) {
-            userRepository.save(user);
+        //User class initial data from JSON file
+        FileReader fileReader = new FileReader("src/main/resources/users.json");
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<User> userList = objectMapper.readValue(fileReader, new TypeReference<List<User>>() {});
+        for(User user : userList){
+            User userToSave = new User(user.getUsername(), passwordEncoder.encode(user.getPassword()));
+            userRepository.save(userToSave);
         }
+        userRepository.save(new User("workshop@tdelektro.pl", adminPassword.getPassword(), "ADMIN"));
 
-        //Tasks for test
-        List<Task> taskList = Arrays.asList(
-                new Task("Car inspection"),
-                new Task("Oil and filter change"),
-                new Task("Tyre change"),
-                new Task("Check coolant"),
-                new Task("Fix coolant leak"),
-                new Task("Check breakes"),
-                new Task("Fix breakes")
-        );
 
-        for (Task task : taskList) {
-            taskRepository.save(task);
-        }
+        //Task class initial data from XML
+        FileReader xmlFile = new FileReader("src/main/resources/tasks.xml");
+        XmlMapper xmlMapper = new XmlMapper();
 
-        FileReader fileReader = new FileReader("src/main/resources/cars.csv");
+        List<Task> taskList = xmlMapper.readValue(xmlFile, List.class);
+        System.out.println(taskList.get(0));
+
+
+        taskRepository.save(taskList.get(0));
+
+        //Car class initial data from CSV file
+        fileReader = new FileReader("src/main/resources/cars.csv");
         CSVReader csvReader = new CSVReader(fileReader);
 
         //Header omit
         csvReader.readNext();
-        String [] stringLine;
+        String[] stringLine;
 
-        while((stringLine = csvReader.readNext())!= null){
+        while ((stringLine = csvReader.readNext()) != null) {
             Car car = new Car(stringLine[0], stringLine[1]);
             carRepository.save(car);
         }
-
-
 
 
     }
